08c84c6d7ac1929e1d610d635b22121798744c93
manuchis <manuchis@gmail.com>
2022-02-23 23:03:42 +0100

    test14

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 import os
-print os.getenv('MQTT_SERVER')
 
+if os.getenv('MQTT_SERVER') == None:
+    print("Error: Please set the environment variable MQTT_SERVER and try again.")
+    exit(1)
+if os.getenv('MQTT_PORT') == None:
+    print("Error: Please set the environment variable MQTT_PORT and try again.")
+    exit(1)
+if os.getenv('BOT_TOKEN') == None:
+    print("Error: Please set the environment variable BOT_TOKEN and try again.")
+    exit(1)
+if os.getenv('BOT_CHAT_ID') == None:
+    print("Error: Please set the environment variable BOT_CHAT_ID and try again.")
+    exit(1)
+    
 MQTT_SERVER = os.getenv('MQTT_SERVER')
 MQTT_PORT = os.getenv('MQTT_PORT')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
