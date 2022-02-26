00c775b486d7b1cc944a1aeb3977907a166dbf4f
manuchis <manuchis@gmail.com>
2022-02-26 18:41:06 +0100

    new implementation

--- teslamateMqttToTelegram.py
+++ teslamateMqttToTelegram.py
@@ -1,225 +1,277 @@
 # -*- coding: utf-8 -*-
 import os
-import paho.mqtt.client as mqtt
 import requests
 import sys
 from time import sleep
-import config as conf
-import logging
-import logging.handlers
 import telebot
+import teslapy
+from datetime import datetime
+from datetime import timedelta
 
-data = {
-    "sent_resumen": 0,
-    "display_name": "",
-    "state": "",
-    "software_current_version": "",
-    "software_new_version": "",
-    "battery_level": 100,
-    "usable_battery_level": 100,
-    "inside_temp": 22,
-    "outside_temp": 22,
-    "longitude": -5,
-    "latitude": 42,
-    "geofence": ""
-}
-botMessage = {
-    "send": 1,
-    "text": ""
-}
-logger = logging.getLogger()
-RESTART = 15
+import os
 
-if conf.OPTIONS != None:
-    OPTIONS = conf.OPTIONS.split("|")
+if os.getenv('USER_EMAIL') == None:
+    print("Error: Please set the environment variable USER_EMAIL and try again.")
+    exit(1)
+else:
+    USER_EMAIL = os.getenv('USER_EMAIL')
+if os.getenv('BOT_TOKEN') == None:
+    print("Error: Please set the environment variable BOT_TOKEN and try again.")
+    exit(1)
+else:
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+if os.getenv('BOT_CHAT_ID') == None:
+    print("Error: Please set the environment variable BOT_CHAT_ID and try again.")
+    exit(1)
+else:
+    BOT_CHAT_ID = os.getenv('BOT_CHAT_ID')
 
-bot = telebot.TeleBot(conf.BOT_TOKEN, parse_mode=None)
+with teslapy.Tesla(USER_EMAIL) as tesla:
+    if not tesla.authorized:
+        tesla.refresh_token(refresh_token=input('Enter SSO refresh token: '))
+    vehicles = tesla.vehicle_list()
+    print(vehicles[0].get_vehicle_summary())
 
-def setup_logging():
-    if conf.DEBUG:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-    handler = logging.handlers.RotatingFileHandler('teslamate2telegram.log', maxBytes=10000000, backupCount=5)
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(funcName)s:%(lineno)s - %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
 
-def get_formated_text(data, message):
+@bot.message_handler(commands=['start'])
+def blue_command(message):
 
-    if data["display_name"] != "":
-        txt = "*{}* {}".format(data["display_name"], message['text'])
-    else:
-        txt = "{}".format(message['text'])
-    return txt
+   keyboard = telebot.types.InlineKeyboardMarkup()
 
-def on_connect(client, userdata, flags, rc):
-    #global botMessage
+   keyboard.row(
+                telebot.types.InlineKeyboardButton('Estado',callback_data='status'),
+                telebot.types.InlineKeyboardButton('Despertar',callback_data='wake_up'),
+                telebot.types.InlineKeyboardButton('Resumen vehículo',callback_data='vehicle_summary'),
+                telebot.types.InlineKeyboardButton('Info',callback_data='info')
+               )
 
-    if conf.DEBUG:
-        logger.info("Connected to the TeslaMate MQTT")
+   keyboard.row(
+                telebot.types.InlineKeyboardButton('Activar sentry',callback_data='sentry_on'),
+                telebot.types.InlineKeyboardButton('Desactivar sentry',callback_data='sentry_off')
+               )
 
-    client.subscribe("teslamate/cars/" + conf.CAR_ID + "/#")
+   keyboard.row(
+                telebot.types.InlineKeyboardButton('Abrir p. carga',callback_data='unlock_charge_port'),
+                telebot.types.InlineKeyboardButton('Cerrar p. carga',callback_data='lock_charge_port'),
+                telebot.types.InlineKeyboardButton('Iniciar carga',callback_data='start_charge'),
+                telebot.types.InlineKeyboardButton('Parar carga',callback_data='stop_charge')
+               )
 
-    message = {
-        "send": 0,
-        "text": "🎉 Ahora estás conectado con _TeslaMate_ 🎉"
-    }
-    bot.reply_to(message, message.text)
+   keyboard.row(
+                telebot.types.InlineKeyboardButton('50%',callback_data='charge50'),
+                telebot.types.InlineKeyboardButton('55%',callback_data='charge55'),
+                telebot.types.InlineKeyboardButton('60%',callback_data='charge60'),
+                telebot.types.InlineKeyboardButton('65%',callback_data='charge65'),
+                telebot.types.InlineKeyboardButton('70%',callback_data='charge70'),
+                telebot.types.InlineKeyboardButton('75%',callback_data='charge75'),
+                telebot.types.InlineKeyboardButton('80%',callback_data='charge80'),
+                telebot.types.InlineKeyboardButton('85%',callback_data='charge85'),
+                telebot.types.InlineKeyboardButton('90%',callback_data='charge90'),
+                telebot.types.InlineKeyboardButton('95%',callback_data='charge95'),
+                telebot.types.InlineKeyboardButton('100%',callback_data='charge100')
+               )
+
+   keyboard.row(
+                telebot.types.InlineKeyboardButton('10A',callback_data='amp10'),
+                telebot.types.InlineKeyboardButton('16A',callback_data='amp16'),
+                telebot.types.InlineKeyboardButton('20A',callback_data='amp20'),
+                telebot.types.InlineKeyboardButton('25A',callback_data='amp25'),
+                telebot.types.InlineKeyboardButton('32A',callback_data='amp32')
+               )
+
+   bot.send_message(message.chat.id,'¡Hola!',reply_markup=keyboard)
+
+def get_callback(query):
+
+   mi = 1.6093345204294200621295255350501
+
+   with teslapy.Tesla(USER_EMAIL) as tesla:
+       if not tesla.authorized:
+           tesla.refresh_token(refresh_token=input('Enter SSO refresh token: '))
+       vehicles = tesla.vehicle_list()
+       vehicle_summary = vehicles[0].get_vehicle_summary()
+
+   bot.answer_callback_query(query.id)
+
+   if query.data == 'wake_up':
+       print('Despertando...')
+       vehicles[0].sync_wake_up()
+       bot.send_message(query.message.chat.id,'🆙')
+       print('Despierto')
+       return
+
+   if query.data == 'status':
+       vehicles = tesla.vehicle_list()
+       vehicle_summary = vehicles[0].get_vehicle_summary()
+
+       if vehicle_summary["state"] == 'online':
+           estado = 'Despierto'
+       if vehicle_summary["state"] == 'asleep':
+           estado = 'Dormido'
+       if vehicle_summary["state"] == 'suspended':
+           estado = 'Dormido'
+       if vehicle_summary["state"] == 'offline':
+           estado = 'No disponible'
+       print('Estado: '+estado)
+
+       bot.send_message(query.message.chat.id,estado)
+       return
+
+   if query.data == 'vehicle_summary':
+      vehicle_summary = vehicles[0].get_vehicle_summary()
+      for i in vehicle_summary:
+          if vehicle_summary[i]:
+             bot.send_message(query.message.chat.id,vehicle_summary[i])
+      return
+
+   if vehicle_summary["state"] != 'online':
+      bot.send_message(query.message.chat.id,"Coche no despierto, hay que despertarlo primero")
+      return
+
+   if query.data == 'info':
+       vehicle_data = vehicles[0].get_vehicle_data()
+       charge = vehicle_data['charge_state']
+       climate = vehicle_data["climate_state"]
+       state = vehicle_data["vehicle_state"]
+
+       if vehicle_data["state"] == 'online':
+           estado = 'Despierto'
+       if vehicle_data["state"] == 'asleep':
+           estado = 'Dormido'
+       if vehicle_data["state"] == 'offline':
+           estado = 'No disponible'
+
+       if state["locked"]:
+           locked = "🔐  Cerrado"
+       else:
+           locked = "🔓  Abierto"
+
+       if charge["usable_battery_level"] != charge["battery_level"]:
+          bat = "Usable {0}% (disponible {1}%) {2:.0f} km".format(charge["usable_battery_level"],charge["battery_level"],round(charge["battery_range"]*mi,0))
+       else:
+          bat = "Disponible {0}% {1:.0f} km".format(charge["usable_battery_level"],round(charge["battery_range"]*mi,0))
+
+       limite = charge["charge_limit_soc"]
+
+       if charge["charging_state"] == "Charging":
+          horas = int(charge["time_to_full_charge"])
+          minutos = (charge["time_to_full_charge"] - horas) * 60
+          total_minutos = horas * 60 + minutos
+          intensidad = charge["charge_amps"]
+          voltaje =  charge["charger_voltage"]
+          fases = charge["charger_phases"]
+          hora_actual = datetime.now()
+          hora_fin_carga = hora_actual + timedelta(minutes=total_minutos)
+
+          if fases == 1:
+             potencia = round(voltaje * intensidad / 1000,1)
+             modo_carga = "monofásica"
+          else:
+             if voltaje != 2:
+                modo_carga = "trifásica"
+                potencia = round(3 * voltaje * intensidad / 1000,1)
+             else:
+                modo_carga = "continua"
+                potencia =  charge["charger_power"]
+
+          estado = estado + " (cargando en " + modo_carga + " a {:.1f} kW)".format(potencia)
+
+       if state["sentry_mode"]:
+          sentry = '🔴 ACTIVADO'
+       else:
+          sentry = '🔘 Desactivado'
+
+       text = " *** Estado de " + state["vehicle_name"] + " ***" \
+              + "\n\n    💿  " + state["car_version"].split()[0] \
+              + "\n    🚗  " + estado \
+              + "\n    ⚙️  {:.0f} km".format(round(state["odometer"]*mi),0) \
+              + "\n    " + locked \
+              + "\n    🔋  {}".format(bat) \
+              + "\n    ⏱  Límite de carga {}%".format(limite)
+
+       if charge["charging_state"] == "Charging":
+          text = text +  "\n    ⏱  Tiempo para finalizar carga: {0}h{1:.0f}m".format(horas,round(minutos,0)) \
+                      +  "\n    ⏱  Hora de finalización carga: " + hora_fin_carga.strftime("%H:%M") \
+                      +  "\n     ⚡️ Voltaje: {0}V".format(voltaje) \
+                      +  "\n     ⚡️ Intensidad: {0}A".format(intensidad) \
+
+       text = text + "\n    🌡️  Interior {0}ºC".format(climate["inside_temp"]) \
+                   + "\n    🌡️  Exterior {0}ºC".format(climate["outside_temp"]) \
+                   + "\n     " + sentry
+
+       #bot.send_photo(query.message.chat.id,
+       bot.send_message(query.message.chat.id,text)
+       return
+
+   if query.data == 'sentry_on':
+       vehicles[0].command('SET_SENTRY_MODE',on='true')
+       bot.send_message(query.message.chat.id,'🔴ACTIVADO')
+       return
+
+   if query.data == 'sentry_off':
+       vehicles[0].command('SET_SENTRY_MODE',on='false')
+       bot.send_message(query.message.chat.id,'🔘desactivado')
+       return
+
+   if query.data == 'unlock_charge_port':
+       vehicles[0].command('CHARGE_PORT_DOOR_OPEN')
+       bot.send_message(query.message.chat.id,'⚡️desbloqueado')
+       return
+
+   if query.data == 'start_charge':
+        vehicles[0].command('START_CHARGE')
+        bot.send_message(query.message.chat.id,'⚡️Carga iniciada')
+        return
+
+   if query.data == 'stop_charge':
+        vehicles[0].command('STOP_CHARGE')
+        bot.send_message(query.message.chat.id,'⚡️Carga parada')
+        return
+
+   if query.data == 'lock_charge_port':
+       vehicles[0].command('CHARGE_PORT_DOOR_CLOSE')
+       bot.send_message(query.message.chat.id,'⚡️bloqueado')
+       return
+
+   if query.data.startswith('charge'):
+       porcentaje = query.data[6:]
+       vehicles[0].command('CHANGE_CHARGE_LIMIT',percent=porcentaje)
+       bot.send_message(query.message.chat.id,'Límite 🔋 fijado al '+porcentaje+'%')
+       return
+
+   if query.data.startswith('amp'):
+       intensidad = query.data[3:]
+       vehicles[0].command('CHARGING_AMPS',charging_amps=intensidad)
+       bot.send_message(query.message.chat.id,'Intensidad ⚡️ fijada a '+intensidad+'A')
+       return
+
+@bot.callback_query_handler(func=lambda call: True)
+def iq_callback(query):
+#   data = query.data
+   get_callback(query)
 
 
-def on_disconnect(client, userdata, rc=0):
-    if conf.DEBUG:
-        logger.debug("Disconnected: result code " + str(rc))
+@bot.message_handler(commands=['carga'])
+def carga_command(message):
+   with teslapy.Tesla(USER_EMAIL) as tesla:
+       if not tesla.authorized:
+           tesla.refresh_token(refresh_token=input('Enter SSO refresh token: '))
+       vehicles = tesla.vehicle_list()
 
-    message = {
-        "send": 0,
-        "text": "(!) MQTT desconectado, se intentará reconectar."
-    }
-    bot.reply_to(message, message.text)
-    client.loop_stop()
-    sleep(RESTART)
-    create_mqtt_connection()
+   porcentaje = message.text[7:]
+   vehicles[0].command('CHANGE_CHARGE_LIMIT',percent=porcentaje)
+   bot.send_message(message.chat.id,'Límite 🔋 fijado al '+porcentaje+'%')
 
+@bot.message_handler(commands=['amp'])
+def amp_command(message):
+   with teslapy.Tesla(USER_EMAIL) as tesla:
+       if not tesla.authorized:
+           tesla.refresh_token(refresh_token=input('Enter SSO refresh token: '))
+       vehicles = tesla.vehicle_list()
 
-def on_message(client, userdata, message):
-    global botMessage
-    global data
+   intensidad = message.text[5:]
+   vehicles[0].command('CHARGING_AMPS',charging_amps=intensidad)
+   bot.send_message(message.chat.id,'Intensidad ⚡️ fijada a '+intensidad+'A')
 
-    try:
-        topic = str(message.topic).split('/')[3]
-        payload = str(message.payload.decode("utf-8"))
-        text = ""
-
-        if topic == "display_name":
-            data["display_name"] = payload
-        elif topic == "version":
-            data["software_current_version"] = payload
-        elif topic == "battery_level":
-            data["battery_level"] = payload
-        elif topic == "usable_battery_level":
-            data["usable_battery_level"] = payload
-        elif topic == "inside_temp":
-            data["inside_temp"] = payload
-        elif topic == "outside_temp":
-            data["outside_temp"] = payload
-        elif topic == "longitude":
-            data["longitude"] = payload
-        elif topic == "latitude":
-            data["latitude"] = payload
-        elif topic == "update_version":
-            if payload != "" and payload != data["software_current_version"]:
-                data["software_new_version"] = payload
-                text = "🎁 Actualización disponible: _{}_".format(payload)
-        elif topic == "state":
-            if data["state"] != payload:
-                if payload == "online":
-                    text = "✨ Está despierto"
-                elif payload == "asleep":
-                    text = "💤 Está dormido"
-                    data['sent_resumen'] = 0
-                elif payload == "suspended":
-                    text = "🛏️ Está durmiéndose"
-                elif payload == "charging":
-                    text = "🔌 Está cargando"
-                elif payload == "offline":
-                    text = "🛰️ No está conectado"
-                elif payload == "start":
-                    text = "🚀 Está arrancando"
-                elif payload == "driving":
-                    text = "🏁 Está conduciendo"
-                else:
-                    text = "⭕ Estado desconocido"
-
-            data["state"] = payload
-        elif topic == "geofence":
-            data["geofence"] = payload
-
-        if conf.DEBUG:
-            logger.debug(topic + ": " + payload)
-
-        if text != "":
-            botMessage = {
-                "send": 0,
-                "text": text
-            }
-
-        if topic in OPTIONS and botMessage['send'] == 0 and botMessage['text'] != "":
-            send_to_telegram()
-
-    except:
-        logger.error("Exception on_message(): ", sys.exc_info()[0], message.topic, message.payload)
-
-
-
-
-
-def send_resume():
-    global botMessage, data
-
-    if conf.DEBUG:
-        logger.info("Send resume to Telegram")
-
-    if data["usable_battery_level"] != data["battery_level"]:
-        bat = "Usable {0}% (disponible {1}%)".format(data["usable_battery_level"], data["battery_level"])
-    else:
-        bat = "Disponible {0}%".format(data["usable_battery_level"])
-
-    if data["geofence"] != "":
-        geo = "Estás en [{0}](https://maps.google.com/maps?q=loc:{1},{2})".format(data["geofence"], data["latitude"], data["longitude"])
-    else:
-        geo = "[Lat: {0}, Long: {1}](https://maps.google.com/maps?q=loc:{0},{1})".format(data["latitude"], data["longitude"])
-
-    text = " -> Resumen:" \
-           + "\n\n    🔋 {}".format(bat) \
-           + "\n    🌡️ Interior {0}ºC".format(data["inside_temp"]) \
-           + "\n    🌡️ Exterior {0}ºC".format(data["outside_temp"]) \
-           + "\n    🌎 {}".format(geo)
-    botMessage = {
-        "send": 0,
-        "text": text
-    }
-    data['sent_resumen'] = 1
-    send_to_telegram()
-
-'''
-def send_to_telegram():
-    global botMessage
-
-    send_text = 'https://api.telegram.org/bot' + conf.BOT_TOKEN + '/sendMessage?chat_id=' + conf.BOT_CHAT_ID + '&parse_mode=Markdown&disable_web_page_preview=True&text=' + get_formated_text()
-    response = requests.get(send_text)
-    if conf.DEBUG:
-        logger.debug(data)
-        logger.debug(response.text)
-
-    botMessage = {"send": 1, "text": ""}
-'''
-
-def create_mqtt_connection():
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.on_disconnect = on_disconnect
-
-    try:
-        if conf.DEBUG:
-            logger.info("Trying to connect to the MQTT")
-
-        client.connect(conf.MQTT_SERVER, int(conf.MQTT_PORT), 60)
-        client.loop_start()
-
-    except (ValueError, Exception):
-        if conf.DEBUG:
-            logger.error("Error trying to connect to the MQTT")
-        sleep(RESTART)
-        create_mqtt_connection()
-
-
-def main():
-    setup_logging()
-    create_mqtt_connection()
-    bot.infinity_polling()
-
-
-if __name__ == '__main__':
-    main()
+bot.infinity_polling()
